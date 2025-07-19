e224dd4fb6c23e18f719ca44298aacbc18b500d7
Parth Dhroovji <parth.dhroovji@gmail.com>
2025-07-19 19:24:26 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.TOPOLOGY({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff6600,
+    backgroundColor: 0x000000
+})
+
