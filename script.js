be9084793eb6fd56e2dc3808d76f0e3db7230bce
Parth Dhroovji <parth.dhroovji@gmail.com>
2025-07-22 02:49:54 -0400

    lost track or changes, background and two boxes added day 2. will pdate better now

--- script.js
+++ script.js
@@ -7,7 +7,36 @@ VANTA.TOPOLOGY({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
+    mobile: true,
     color: 0xff6600,
     backgroundColor: 0x000000
-})
+});
 
+
+var i = 0;
+var txt = ["computer science major.", "AI enthusiast.", "software developer.", "team leader.", "passionate learner."];
+var speed = 100;
+var desc = 0;
+
+function iAmTypeWriter() {
+    if (i < txt[desc].length){
+        document.getElementById("heropara").innerHTML += txt[desc].charAt(i);
+        i++;
+        setTimeout(iAmTypeWriter, speed);
+    }
+    else if (i == txt[desc].length) {
+        setTimeout(resetTypeWriter, 2000);
+    }
+}
+
+function resetTypeWriter() {
+    desc++;
+    if (desc >= txt.length) {
+        desc = 0;
+    }
+    i = 0;
+    document.getElementById("heropara").innerHTML = "";
+    setTimeout(iAmTypeWriter, speed);
+}
+
+iAmTypeWriter();
